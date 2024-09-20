99027f0b5bfe8b6d7603d6adc2af686dd74734ef
CaioLopeszz <LopesCaiio@outlook.com>
2024-09-20 10:27:50 -0300

    Final Project

---
+++ Calculadora.js
@@ -0,0 +1,36 @@
+let resultado = calcularRankeada(91, 10)
+
+console.log(resultado)
+
+function calcularRankeada(numVitorias, numDerrotas){
+  
+    const saldoRankeada = numVitorias - numDerrotas
+
+    let nivelRankeada
+
+    if (numVitorias <= 10){
+        nivelRankeada = "Ferro";
+    }
+    else if (numVitorias >= 11 && numVitorias <= 20){
+        nivelRankeada = "Bronze";
+    }
+    else if (numVitorias >= 21 && numVitorias <= 50){
+        nivelRankeada = "Prata";
+    }
+    else if (numVitorias >= 51 && numVitorias <= 80){
+        nivelRankeada = "Ouro";
+    }
+    else if (numVitorias >= 81 && numVitorias <= 90){
+        nivelRankeada = "Diamante";
+    }
+    else if (numVitorias >= 91 && numVitorias <= 100){
+        nivelRankeada = "Lendário";
+    }
+    else if (numVitorias >= 101){
+        nivelRankeada = "Imortal";
+    }
+
+    return "O Herói tem um saldo de " + saldoRankeada + " e está no nível de " + nivelRankeada
+}
+
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# 💻Calculadora de Partidas Rankeadas
+
+ Nesse projeto, fizemos um **calculadora de partidas rankeadas** que recebe como parâmetro a **"quantidade de vitórias e derrotas"** de um jogador, e define o seu **nível de rankeada**, utilizando todos os conhecimentos de **lógica de programação** que adquirimos ao longo das aulas. 
+
+ Curso **Formação Lógica de Programação**, feito pela [Digital Innovation One](https://www.dio.me/).
